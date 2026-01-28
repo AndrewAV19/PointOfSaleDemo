b9abc3aceed4cc06931d3a5a3445db76fc251095
Andrew Emmanuel Alonso Valenzuela <andrew.alonso@citi.com.mx>
2026-01-28 17:05:57 -0600

    UPDATE

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    target: 'es2020'
+    sourcemap: true
   },
-  plugins: [react()],
-})
+  server: {
+    port: 3000
+  }
+});
